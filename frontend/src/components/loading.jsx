21e49a430d05d7661c323ee987ee70e0ad58e3ef
nagesh-merva <officialnagesh.merva@gmail.com>
2024-07-20 17:36:25 +0530

    Update loading.jsx

--- frontend/src/components/loading.jsx
+++ frontend/src/components/loading.jsx
@@ -1,14 +1,9 @@
-import { XlviLoader } from "react-awesome-loaders"
+
 
 function Loading() {
     return (
         <div className="h-full w-full flex justify-center items-center">
-            <XlviLoader
-                boxColors={["#EF4444", "#F59E0B", "#6366F1"]}
-                style={{ marginBottom: "20px" }}
-                desktopSize={"60px"}
-                mobileSize={"80px"}
-            />
+
         </div>
     )
 }
