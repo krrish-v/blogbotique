e461aa23bf61e3dfb482396ac559b34caa84e9b2
krrish <87867238+krrish-v@users.noreply.github.com>
2024-07-25 12:46:54 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -3,10 +3,6 @@ from flask import Flask, jsonify, render_template, request
 from workflow import Backend
 import json
 
-# Create an instance of the Backend class
-
-backend = Backend()
-index = None
 
 
 app = Flask(__name__)
@@ -14,38 +10,42 @@ app = Flask(__name__)
 CORS(app, supports_credentials=True, allow_headers="*", origins="*", methods=["OPTIONS", "POST"])
 
 
+backend = Backend()
+index = None
+
+'''
 @app.route('/')
 def index():
     return render_template('index.html')
-
+'''
 
 @app.route('/upload/url', methods=['POST'])
 def upload_url():
-    if request.method == "POST":
-        url = request.form['url']
-        print(url)
-        
-        keywords_json_string = backend.get_keywords(url)
-        index = backend.index_manager
 
-        return jsonify({'status': 'success', 'data':keywords_json_string})
+    if request.method == 'OPTIONS':
+        return jsonify({'status': 'success', 'message': 'CORS preflight request handled successfully'}), 200
+    
+    if request.is_json:
+
+        url = request.json['link']
+
+        keywords_json_string = backend.get_keywords(url)
+        keywords_json = json.loads(keywords_json_string)["keywords"]
+        print(keywords_json)
+
+        return jsonify({'status': 'success', 'keywords':keywords_json})
 
     return False
 
 @app.route('/api/generatetitles', methods=['POST'])
 def generate_title():
+
     if request.method == "POST":
         
-        keywords = request.form['keywords']
-
-        print(type(keywords), keywords)
-
-        #keywords_json = json.loads(keywords)
-        #keyword__ = keywords_json['keywords']
-
-        #titles = backend.get_titles(keyword__, index)
-        titles = None
-
+        json_data = request.json['selectedKeywords']
+        print(json_data)
+        titles = backend.get_titles(json_data, index)
+        
         return jsonify({'status': 'success','titles':titles})
 
     return False 
@@ -53,18 +53,49 @@ def generate_title():
 
 @app.route('/api/generateblog', methods=['POST'])
 def generate_blog():
+
     if request.method == "POST":
-        title = request.form['title']
-        keywords = request.form['keywords']
-        custom_prompt = request.form['custom_prompt']
+        title = request.json['title']
+        keywords = request.json['tags']
+        custom_prompt = request.json['custom_prompt']
+        print(title, keywords, custom_prompt)
 
         blog = backend.get_blog(title, custom_prompt, keywords, index)
+        print(blog)
 
         return jsonify({'status': 'success','blog':blog})
 
     return False 
 
 
+@app.route('/api/enhanceblog', methods=['POST'])
+def enhance_blog():
+
+    if request.method == "POST":
+        custom_prompt = request.json['custom_prompt']
+        blog = request.json['blog']
+        print(custom_prompt)
+
+        blog = backend.enhance_blog(blog, custom_prompt)
+        print(blog)
+
+        return jsonify({'status': 'success','blog':blog})
+
+    return False 
+
+
+@app.route('/api/getcode', methods=['POST'])
+def blog_code():
+
+    if request.method == "POST":
+        blog = request.json['blog']
+
+        code = backend.code_of_blog(blog)
+        print(code)
+
+        return jsonify({'status': 'success','code':code})
+
+    return False 
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080, debug=True)
